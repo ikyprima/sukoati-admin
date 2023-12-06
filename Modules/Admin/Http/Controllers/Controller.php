48d716e4ff49b001cfd3aef1160e1fdec89e35aa
Rizky Primadona <primadona.rizky22@gmail.com>
2023-12-06 16:59:03 +0700

    perbaikan jika request null

--- Modules/Admin/Http/Controllers/Controller.php
+++ Modules/Admin/Http/Controllers/Controller.php
@@ -10,12 +10,20 @@ use Illuminate\Routing\Controller as BaseController ;
 class Controller extends BaseController
 {
     public function getSlug(Request $request){
-        if (isset($this->slug)) {
-            $slug = $this->slug;
-        } else {
-            $slug = explode('.', $request->route()->getName())[0];
-        }
+        // if (isset($this->slug)) {
+        //     $slug = $this->slug;
+        // } else {
+        //     $slug = explode('.', $request->route()->getName())[0];
+        // }
 
+        // return $slug;
+        if ($request->route() && $request->route()->getName()) {
+            $slug = explode('.', $request->route()->getName())[0];
+        } else {
+            // Default jika route atau getName() null
+            $slug = 'default';  // Gantilah dengan nilai default yang sesuai
+        }
+    
         return $slug;
     }
 }
