09db642ab71d4155469207b2644dff06c8be8f39
Rizky Primadona <primadona.rizky22@gmail.com>
2023-12-07 16:58:29 +0700

    tambah tabel yang di exclude

--- Modules/Admin/Config/config.php
+++ Modules/Admin/Config/config.php
@@ -19,5 +19,13 @@ return [
         'migrations',
         'data_rows',
         'data_types',
-        'failed_jobs'] //list table yang di blacklist dari builder
+        'failed_jobs',
+        'table_barang',
+        'tb_agama','tb_eselon','tb_jabatan',
+        'tb_jenis_identitas','tb_jenis_kelamin',
+        'tb_jenis_kontak','tb_kategori',
+        'tb_pangkat',
+        'tb_pegawai','tb_pegawai_alamat',
+        'tb_pegawai_identitas',
+        'tb_pegawai_kontak','tb_pegawai_pendidikan','tb_pendidikan','tb_satuan'] //list table yang di blacklist dari builder
 ];
